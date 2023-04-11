3cafe9859f0677f6015dbd2b659cb983406defa1
mojnovse <karagodin.kirill.a@ya.ru>
2023-04-11 13:17:08 +0300

    upgrade provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,17 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "karagodin-project-bucket"
+    region     = "ru-central1"
+    key        = "terraform.tfstate"
+    access_key = "YCAJEgWhukwPlmy7VCGduYfaT"
+    secret_key = "YCMP423X7Vi2TSVMjJDj1yh2l_S1fOWF21svclAk"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
 
 provider "yandex" {
